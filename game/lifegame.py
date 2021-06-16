0e2bea058e7e0656bb40496ff9c16abe551264d8
skuralll <f20154@e.hiroshima-cu.ac.jp>
2021-06-16 16:47:53 +0900

    オートモードの追加

--- game/lifegame.py
+++ game/lifegame.py
@@ -33,7 +33,9 @@ class GUI:
     CELL_SIZE = 16
     CELL_WIDTH = 1
     # UI
-    BLANK_WIDTH = 128
+    BLANK_WIDTH = 256
+    BUTTON_AUTORUN_TEXT_OFF = 'オートラン'
+    BUTTON_AUTORUN_TEXT_ON = 'オートラン(停止)'
 
     def __init__(self, lifegame):
         self.app = tk.Tk()
@@ -49,6 +51,12 @@ class GUI:
         # UIs
         self.stepButton = tk.Button(self.app, text="進む", width=15, height=1, command=self.lifegame.step)
         self.stepButton.place(x=width, y=0)
+        self.autoMode = False
+        self.autoMode_period = 333
+        self.autoButton = tk.Button(self.app, text=self.BUTTON_AUTORUN_TEXT_OFF, width=15, height=1, command=self.clickAutoButton)
+        self.autoButton.place(x=width, y=40)
+        self.SpeedScale = tk.Scale(self.app, from_=1, to=10, orient=tk.HORIZONTAL, command=self.onSpeedScaleChanged)
+        self.SpeedScale.place(x=width + 128, y=28)
         # Canvas
         self.canvas = tk.Canvas(self.app, width=width, height=height, bg="white", highlightthickness=0)
         self.canvas.place(x=0, y=0)
@@ -79,8 +87,24 @@ class GUI:
         fill = "red" if self.lifegame.board.cells[row][col].isAlive() else "white"
         self.canvas.itemconfig(self.colmToTag(row, col), fill=fill)
 
-    def autoStep(self, interval: int):
-        pass
+    # callbackTask
+    def autoStep(self):
+        if self.autoMode:
+            self.lifegame.step()
+            self.app.after(self.autoMode_period, self.autoStep)
 
     def colmToTag(self, row: int, col: int):
         return 'cell' + str(row) + ':' + str(col)
+
+    # UIs
+    # AutoButton
+    def clickAutoButton(self):
+        self.autoMode = not self.autoMode
+        if self.autoMode:
+            self.autoStep()
+            self.autoButton.configure(text=self.BUTTON_AUTORUN_TEXT_ON)
+        else:
+            self.autoButton.configure(text=self.BUTTON_AUTORUN_TEXT_OFF)
+    # SpeedScale
+    def onSpeedScaleChanged(self, scale_val):
+        self.autoMode_period = int(1000 / int(scale_val))
